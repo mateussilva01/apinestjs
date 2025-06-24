83fa23ebe7ea82046d2bbf6b84fd5afde30f8308
mateussilva01 <mateussilva.dev01@gmail.com>
2025-06-24 20:10:34 -0300

    Integração do TypeORM com PostgreSQL para gerenciamento de entidades.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeormConfigService } from './config/typeorm.config.service';
 import { BancoProvider } from './database/banco.provider';
+import { AuthModule } from './auth/auth.module';
 import { FilmeModule } from './modules/filme/filme.module';
 import { DiretorModule } from './modules/filme/diretor/diretor.module';
 import { AtorModule } from './modules/filme/elenco/ator/ator.module';
 import { GeneroModule } from './modules/filme/genero/genero.module';
 import { InformacaoModule } from './modules/informacao/informacao.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), 
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeormConfigService,
+      inject: [TypeormConfigService],
+    }),
     JwtModule,
     AuthModule,
     FilmeModule, 
